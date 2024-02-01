6631678bf2145665f5924ab4e0b4c02879438bf4
jchenprogramming <157077133+jchenprogramming@users.noreply.github.com>
2024-01-31 21:17:37 -0500

    Update samanalysis.sh

--- src/searcher/samanalysis.sh
+++ src/searcher/samanalysis.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-# Loop through each index for A
+# Array to store background process IDs
+bg_pids=()
+
+# Loop through each index for A and run in the background
 for i in {1..10}; do
-  ./arunner.sh $i
+  ./arunner.sh $i &
+  # Store the background process ID in the array
+  bg_pids+=($!)
 done
 
-# Loop through each index for B
+# Loop through each index for B and run in the background
 for i in {1..10}; do
-  ./brunner.sh $i
+  ./brunner.sh $i &
+  # Store the background process ID in the array
+  bg_pids+=($!)
 done
+
+# Wait for all background processes to complete
+for pid in "${bg_pids[@]}"; do
+    wait "$pid"
+done
+
+# Execute the next step after all processes are done
+cd ..
+cd cleaner
+bash results.sh
