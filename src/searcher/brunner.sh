a31da05e0c97b16c7c8b6b938a82f33effa0b2bc
jchenprogramming <157077133+jchenprogramming@users.noreply.github.com>
2024-01-31 21:31:16 -0500

    Update brunner.sh

--- src/searcher/brunner.sh
+++ src/searcher/brunner.sh
@@ -2,9 +2,9 @@
 
 # Loop through each chromosome
 for i in "$@"; do
-    REF="BChrom${i}.txt"
-    DICT="SNPChrom${i}.txt"
-    OUTPUT="BFinalChrom${i}.txt"
+    REF="../../output_files/BChrom${i}.txt"
+    DICT="../../output_files/SNPChrom${i}.txt"
+    OUTPUT="../../output_files/BFinalChrom${i}.txt"
 
     # Run Java program using here document
     java SamSearcher2 <<EOF
