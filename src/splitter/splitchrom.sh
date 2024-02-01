3bb7617e650ccb1524d31318175c0c21a2f19ad6
jchenprogramming <157077133+jchenprogramming@users.noreply.github.com>
2024-01-31 21:29:46 -0500

    Update splitchrom.sh

--- src/splitter/splitchrom.sh
+++ src/splitter/splitchrom.sh
@@ -6,4 +6,5 @@ output_file1=$3
 output_file2=$4
 
 # Run Java program to split the file
-java FileSplitter "$input_file" "$output_file1" "$output_file2"
+java FileSplitter "../../input_files/$input_file" "../../output_files/$output_file1" "../../output_files/$output_file2"
+
