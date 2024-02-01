dd09ac7377e870d2beb6dbca884acd5ac4fb61ca
jchenprogramming <157077133+jchenprogramming@users.noreply.github.com>
2024-01-31 21:15:35 -0500

    Update mastersplit.sh

--- src/splitter/mastersplit.sh
+++ src/splitter/mastersplit.sh
@@ -13,6 +13,22 @@ java ChromSplitter
 java SamSplitter
 
 # Loop through each chromosome and execute the splitchrom.sh script
+# Array to store background process IDs
+bg_pids=()
+
+# Loop through each chromosome and execute the splitchrom.sh script in the background
 for i in {1..10}; do
-    bash splitchrom.sh "Chrom${i}.txt" "AChrom${i}.txt" "BChrom${i}.txt"
+    bash splitchrom.sh "Chrom${i}.txt" "AChrom${i}.txt" "BChrom${i}.txt" &
+    # Store the background process ID
+    bg_pids+=($!)
 done
+
+# Wait for all background processes to complete
+for pid in "${bg_pids[@]}"; do
+    wait "$pid"
+done
+
+# All background processes have completed
+cd ..
+cd searcher
+bash samanalysis.sh
